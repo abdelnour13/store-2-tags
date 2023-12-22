80e3d3ceca1fd325f3bda89824555ab78eed0084
abdelnour13 <ab.fellah@esi-sba.dz>
2023-12-22 23:42:04 +0100

    data cleaning

---
+++ utils.py
@@ -0,0 +1,14 @@
+import json
+import pandas as pd
+
+def load_dataset(path: str):
+    df = pd.read_csv(path)
+    df["labels"] = df["labels"].apply(json.loads)
+    df["mapped labels"] = df["mapped labels"].apply(json.loads)
+    df.fillna("")
+    return df
+
+def save_dataset(df: pd.DataFrame, path: str):
+    df["labels"] = df["labels"].apply(json.dumps)
+    df["mapped labels"] = df["mapped labels"].apply(json.dumps)
+    df.to_csv(path)
