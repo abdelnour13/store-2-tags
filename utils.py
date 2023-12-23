49865650725933528c65c97fc108781d92b50b74
abdelnour13 <ab.fellah@esi-sba.dz>
2023-12-23 18:41:53 +0100

    utils

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def load_dataset(path: str):
     df = pd.read_csv(path)
     df["labels"] = df["labels"].apply(json.loads)
     df["mapped labels"] = df["mapped labels"].apply(json.loads)
-    df.fillna("")
+    df.fillna("",inplace=True)
     return df
 
 def save_dataset(df: pd.DataFrame, path: str):
