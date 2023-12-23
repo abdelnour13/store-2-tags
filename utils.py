274a4ce1e96af986e68f69e03c5ece1ef78ed7a7
abdelnour13 <ab.fellah@esi-sba.dz>
2023-12-23 18:23:23 +0100

    adding dataset

--- utils.py
+++ utils.py
@@ -11,4 +11,4 @@ def load_dataset(path: str):
 def save_dataset(df: pd.DataFrame, path: str):
     df["labels"] = df["labels"].apply(json.dumps)
     df["mapped labels"] = df["mapped labels"].apply(json.dumps)
-    df.to_csv(path)
+    df.to_csv(path, index=False)
